d055543e7dacfe0fdd139e15a249a02d06f7ae5b
Tegar Lucky'Q Oakley <55655976+tegaroakley@users.noreply.github.com>
2019-09-24 15:55:14 +0900

    Add files via upload

---
+++ src/pbo2/pkg10118066/latihan9/teks/PBO210118066Latihan9Teks.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118066.latihan9.teks;
+
+/**
+ *
+ * @author 
+ * NAMA  : Tegar Lucky'Q Oakley 
+ * NIM   : 10118066 
+ * KELAS : IF2 
+ * Deskripsi Program : Program ini berisi program untuk Menampilkan Teks
+ */
+public class PBO210118066Latihan9Teks {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        char c;
+        char tab = '\t';
+        char newline ='\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char: " + tab + "berisi " + c);
+    }
+    
+}
